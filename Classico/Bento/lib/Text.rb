52b8c55bfbd704d0a8458fe6290bd449d9cd1209
rafie <rafie@avaya.com>
2015-05-04 20:10:42 +0300

    ...

--- Classico/Bento/lib/Text.rb
+++ Classico/Bento/lib/Text.rb
@@ -37,3 +37,20 @@ end
 end # Text
 
 end # Bento
+
+#----------------------------------------------------------------------------------------------
+
+class String
+	def indent(count, char = ' ')
+		gsub(/([^\n]*)(\n|$)/) do |match|
+			last_iteration = $1 == "" && $2 == ""
+			line = ""
+			line << (char * count) unless last_iteration
+			line << $1
+			line << $2
+			line
+		end
+	end
+end
+
+#----------------------------------------------------------------------------------------------
